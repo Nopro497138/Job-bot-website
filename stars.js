29239ca3f9387b7ceb28b21e76b16324d40ec420
Nopro497138 <noahjovanovic96@gmail.com>
2025-07-20 23:46:13 +0300

    Add files via upload

---
+++ stars.js
@@ -0,0 +1,35 @@
+
+const canvas = document.createElement('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+document.getElementById('stars').appendChild(canvas);
+const ctx = canvas.getContext('2d');
+
+let stars = [];
+for (let i = 0; i < 200; i++) {
+  stars.push({
+    x: Math.random() * canvas.width,
+    y: Math.random() * canvas.height,
+    radius: Math.random() * 1.5,
+    alpha: Math.random(),
+    dx: (Math.random() - 0.5) * 0.5,
+    dy: (Math.random() - 0.5) * 0.5
+  });
+}
+
+function animateStars() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  stars.forEach(star => {
+    ctx.beginPath();
+    ctx.arc(star.x, star.y, star.radius, 0, Math.PI * 2);
+    ctx.fillStyle = 'rgba(51, 204, 255,' + star.alpha + ')';
+    ctx.fill();
+    star.x += star.dx;
+    star.y += star.dy;
+
+    if (star.x < 0 || star.x > canvas.width) star.dx *= -1;
+    if (star.y < 0 || star.y > canvas.height) star.dy *= -1;
+  });
+  requestAnimationFrame(animateStars);
+}
+animateStars();
